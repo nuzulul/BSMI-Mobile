756befb5eb3aae60a92a237531f0e2d74337b593
nuzulul <nuzulz@gmail.com>
2020-01-03 17:31:55 +0700

    versi 2.0.0 new

---
+++ cordovabuildandroiddebug.bat
@@ -0,0 +1,6 @@
+call cordova platform remove android
+call cordova platform add android
+del %~dp0platforms\android\CordovaLib\src\org\apache\cordova\PermissionHelper.java
+call cordova build android
+copy /Y %~dp0platforms\android\app\build\outputs\apk\debug\app-debug.apk %~dp0app-debug.apk
+pause
---
+++ cordovabuildandroidrelease.bat
@@ -0,0 +1,6 @@
+call cordova platform remove android
+call cordova platform add android
+del %~dp0platforms\android\CordovaLib\src\org\apache\cordova\PermissionHelper.java
+call cordova build android --release android
+copy /Y %~dp0platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk %~dp0app-release-unsigned.apk
+pause
