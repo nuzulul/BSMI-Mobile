d06d47cd36d0783de049ea530105d7681970a72d
nuzulul <nuzulz@gmail.com>
2023-08-23 13:56:43 +0700

    test

--- cordovabuildandroidrelease.bat
+++ cordovabuildandroidrelease.bat
@@ -1,8 +1,8 @@
 call cordova telemetry off
 call cordova platform remove android
-call cordova platform add android
+call cordova platform add android@12.0.0
 rem call cordova prepare
-del %~dp0platforms\android\CordovaLib\src\org\apache\cordova\PermissionHelper.java
+rem del %~dp0platforms\android\CordovaLib\src\org\apache\cordova\PermissionHelper.java
 call cordova build android --release android
 copy /Y %~dp0platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk %~dp0app-release-unsigned.apk
 pause
