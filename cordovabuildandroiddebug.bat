0478858cfbfc7ecf653f4d52a657dd765f260ffb
nuzulul <nuzulz@gmail.com>
2023-08-23 13:48:06 +0700

    test

--- cordovabuildandroiddebug.bat
+++ cordovabuildandroiddebug.bat
@@ -1,7 +1,7 @@
 call cordova telemetry off
 call cordova platform remove android
-call cordova platform add android
-del %~dp0platforms\android\CordovaLib\src\org\apache\cordova\PermissionHelper.java
+call cordova platform add android@12.0.0
+rem del %~dp0platforms\android\CordovaLib\src\org\apache\cordova\PermissionHelper.java
 call cordova build android
 copy /Y %~dp0platforms\android\app\build\outputs\apk\debug\app-debug.apk %~dp0app-debug.apk
 pause
